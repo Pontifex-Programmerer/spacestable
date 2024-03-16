ba5c6424d6be08d1b448008d3671745f743f67f5
Geir Hilmersen <geir.hilmersen@protonmail.com>
2024-03-16 15:44:19 +0100

    adding first iteration with planned and dev-tested funstionality

---
+++ index.js
@@ -0,0 +1,21 @@
+//Global imports
+const express = require('express');
+
+//local imports
+const router = require('./router/default-routes');
+
+//Initializing constants
+const app = express();
+const PORT = 3000 | process.env.PORT;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(router);
+
+
+console.log(`Building stable environment for spaceponies..`);
+
+app.listen(PORT, ()=>{
+    console.log(`Spacepony stable build completed. It can now be found @ port ${PORT}`);
+    console.log('Go get those ponies! Be a good ponyboy!');
+});
